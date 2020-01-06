ece3a5f42adb8fc41be64b69f0e59905e855457a
daniel-yl-fan <daniel_qdcn@hotmail.com>
2020-01-06 08:13:48 -0500

    cheat

--- cheatsheets.bash
+++ cheatsheets.bash
@@ -1,1 +1,2 @@
-git clone git@github.com:cheat/cheatsheets.git /root/cheat/community
+git clone git@github.com:cheat/cheatsheets.git /media/sf_Documents/cheat/community
+git clone https://github.com/skywind3000/awesome-cheatsheets.git /media/sf_Documents/cheat/skywind3000
